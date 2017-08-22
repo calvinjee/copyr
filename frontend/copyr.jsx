7d50f219a70410b9fd8d653524c403afab9670d2
calvinjee <cal.jee7@gmail.com>
2017-08-22 11:27:11 -0400

    Add store

--- frontend/copyr.jsx
+++ frontend/copyr.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+
+//testing
 import * as APIUtil from './util/session_api_util';
 
 window.createUser = APIUtil.createUser;
@@ -9,5 +12,6 @@ window.logout = APIUtil.logout;
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
+  const store = configureStore()
   ReactDOM.render(<h1>copyr</h1>, root);
 });
