68ee17956039f271fe08a31791a7d43357e1719b
calvinjee <cal.jee7@gmail.com>
2017-08-22 11:16:42 -0400

    Add session reducer

---
+++ frontend/reducers/session_reducer.js
@@ -0,0 +1,19 @@
+import {
+  RECEIVE_CURRENT_USER,
+  RECEIVE_ERRORS } from '../actions/session_actions';
+import { merge } from 'lodash';
+
+const defaultState = { currentUser: {}, errors: [] };
+
+const sessionReducer = (state = defaultState, action) => {
+  Object.freeze(state);
+  switch(action.type) {
+    case RECEIVE_CURRENT_USER:
+      return merge({}, state, action.currentUser );
+    case RECEIVE_ERRORS:
+      return merge({}, state, action.errors);
+    default: return state;
+  }
+};
+
+export default sessionReducer;
