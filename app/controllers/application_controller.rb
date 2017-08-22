445b4e89cecb9c0e84e1da2e1deecb3399a732b1
calvinjee <cal.jee7@gmail.com>
2017-08-22 09:46:44 -0400

    Update application_controller with user auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,23 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def current_user
+    return nil unless session[:session_token]
+    @current_user || User.find_by(session_token: session[:session_token])
+  end
+
+  def login(user)
+    session[:session_token] = user.reset_session_token
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def logout
+    current_user.reset_session_token
+    session[:session_token] = nil
+    @current_user = nil
+  end
 end
