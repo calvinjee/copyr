31a0f579a3fac42777eebb7c1d56275cc8a03057
calvinjee <cal.jee7@gmail.com>
2017-08-23 17:07:09 -0400

    Update posts reducer

--- frontend/reducers/posts_reducer.js
+++ frontend/reducers/posts_reducer.js
@@ -1,6 +1,7 @@
-import RECEIVE_ALL_POSTS from '../actions/post_actions';
-import RECEIVE_FIVE_POSTS from '../actions/post_actions';
-import RECEIVE_SINGLE_POST from '../actions/post_actions';
+import { RECEIVE_ALL_POSTS,
+  RECEIVE_FIVE_POSTS,
+  RECEIVE_SINGLE_POST,
+  REMOVE_POST } from '../actions/post_actions';
 import { merge } from 'lodash';
 
 
@@ -12,6 +13,11 @@ const postsReducer = (state = {}, action) => {
     case RECEIVE_SINGLE_POST:
       const post = action.post;
       return merge({}, state, { post });
+    case REMOVE_POST:
+      let newState = merge({}, state);
+      delete newState[action.post.id];
+      return newState;
+      // TODO: make sure to remove post ids from liked post ids elsewhere in state
     default: return state;
   }
 };
