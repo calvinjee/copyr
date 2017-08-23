bce9913910714b3760ecb81a493216965ccaca1e
calvinjee <cal.jee7@gmail.com>
2017-08-23 15:09:25 -0400

    Add post actions for receiving/requesting all/single post(s)

--- frontend/actions/post_actions.js
+++ frontend/actions/post_actions.js
@@ -1,3 +1,6 @@
+import * as APIUtil from '../util/posts_util';
+import { receiveErrors } from './session_actions';
+
 export const RECEIVE_ALL_POSTS = 'RECEIVE_ALL_POSTS';
 export const RECEIVE_FIVE_POSTS = 'RECEIVE_FIVE_POSTS';
 export const RECEIVE_SINGLE_POST = 'RECEIVE_SINGLE_POST';
@@ -8,17 +11,37 @@ export const receiveAllPosts = (posts) => {
     posts: posts,
   };
 };
-
-export const receiveFivePosts = (posts) => {
-  return {
-    type: RECEIVE_FIVE_POSTS,
-    posts: posts,
-  };
-};
+//
+// export const receiveFivePosts = (posts) => {
+//   return {
+//     type: RECEIVE_FIVE_POSTS,
+//     posts: posts,
+//   };
+// };
 
 export const receiveSinglePost = (post) => {
   return {
     type: RECEIVE_SINGLE_POST,
     post: post,
+  };
+};
+
+export const requestAllPosts = () => {
+  return (dispatch) => {
+    return APIUtil.fetchAllPosts()
+      .then(
+        (posts) => { return dispatch(receiveAllPosts(posts)); },
+        (errors) => { return dispatch(receiveErrors(errors)); }
+      );
+  };
+};
+
+export const requestSinglePost = (id) => {
+  return (dispatch) => {
+    return APIUtil.fetchSinglePosts(id)
+      .then(
+        (post) => { return dispatch(receiveSinglePost(post)); },
+        (errors) => { return dispatch(receiveErrors(errors)); }
+      );
   };
 };
---
+++ frontend/reducers/posts_reducer.js
@@ -0,0 +1,3 @@
+import RECEIVE_ALL_POSTS from '../actions/post_actions';
+import RECEIVE_FIVE_POSTS from '../actions/post_actions';
+import RECEIVE_SINGLE_POST from '../actions/post_actions';
