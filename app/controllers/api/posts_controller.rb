fb15420a05b107b341697d9f3325082ce417882c
calvinjee <cal.jee7@gmail.com>
2017-08-23 14:18:22 -0400

    Add routes for posts and posts controller

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -17,17 +17,21 @@ class Api::UsersController < ApplicationController
     render 'api/users/show'
   end
 
-  # def update
-  #   if current_user
-  #     if current_user.update(user_params)
-  #       render json: current_user
-  #     else
-  #       render json: current_user.errors.full_messages, status: 422
-  #     end
-  #   else
-  #     render json: ['Need to be logged in to update'], status: 422
-  #   end
-  # end
+  def update
+    if current_user
+      if current_user.update(user_params)
+        render json: current_user
+      else
+        render json: current_user.errors.full_messages, status: 422
+      end
+    else
+      render json: ['Need to be logged in to update'], status: 422
+    end
+  end
+
+  def delete
+  end
+
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -58,6 +58,8 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     resources :users, only: [:create, :show, :update]
     resource :session, only: [:create, :destroy]
+
+    resources :posts, only: [:index, :create, :show, :update, :delete]
   end
 
   root to: 'static_pages#root'
