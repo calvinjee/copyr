f1af92ea522d69319b45159e824210f32f3a5941
calvinjee <cal.jee7@gmail.com>
2017-08-23 21:41:42 -0400

    Populate part of text form component

--- frontend/components/post_forms/text_form.jsx
+++ frontend/components/post_forms/text_form.jsx
@@ -4,12 +4,30 @@ import FormContainer from './form_container';
 class TextForm extends React.Component {
   constructor(props) {
     super(props);
+    this.state({
+      title: null,
+      text_content: null,
+      content_type: 'text'
+    });
   }
 
   render() {
     return(
       <div>
-        <p className="username-head"></p>
+        <p className="username-head">{this.props.currentUser.username}</p>
+        <textarea
+          className="auth-input auth-email"
+          placeholder="Title"
+          value={this.state.email}
+          onChange={this.handleChange('email')} />
+        <textarea
+          className="auth-input auth-email"
+          placeholder="Body"
+          value={this.state.email}
+          onChange={this.handleChange('email')} />
+
+
+
       </div>
     );
   }
