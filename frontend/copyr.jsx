4688f19d6b14d12a026183911fc0abbd7f550c0e
calvinjee <cal.jee7@gmail.com>
2017-09-01 08:56:25 -0400

    Clean up entry file

--- frontend/copyr.jsx
+++ frontend/copyr.jsx
@@ -2,13 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
-//testing
-import * as APIUtil from './util/session_api_util';
-import { signup, login, logout } from './actions/session_actions';
-import { values } from 'lodash';
-// import { openModal } from './actions/modal_actions';
-import { requestAllPosts, requestSinglePost, deletePost, addPost, revisePost } from './actions/post_actions';
-import { merge } from 'lodash';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
@@ -20,22 +13,6 @@ document.addEventListener('DOMContentLoaded', () => {
   } else {
     store = configureStore();
   }
-
-  // testing
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
-  // window.signup = signup;
-  // window.login = login;
-  window.logout = logout;
-  // window.openModal = openModal;
-  window.merge = merge;
-  window.requestAllPosts = requestAllPosts;
-  window.requestSinglePost = requestSinglePost;
-  window.deletePost = deletePost;
-  window.addPost = addPost;
-  window.revisePost = revisePost;
-  window.lodashvalues = values;
-  //
 
   ReactDOM.render(<Root store={store} />, root);
 });
