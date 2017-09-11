97fd0fad31510f3cd0215b0f29e130a58ab9ef3b
calvinjee <cal.jee7@gmail.com>
2017-09-11 17:54:47 -0400

    Update wepack to remove minification warnings

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,25 @@
 const path = require('path');
+const webpack = require("webpack");
+
+let plugins = []; // if using any plugins for both dev and production
+const devPlugins = []; // if using any plugins for development
+
+const prodPlugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: true
+    }
+  })
+];
+
+plugins = plugins.concat(
+  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+);
 
 module.exports = {
   context: __dirname,
@@ -7,6 +28,7 @@ module.exports = {
     path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
     filename: 'bundle.js'
   },
+  plugins: plugins,
   resolve: {
     extensions: ['.js', '.jsx', '*']
   },
