5364983382651a77bca454d47c5d64e6e909d6df
calvinjee <cal.jee7@gmail.com>
2017-09-11 17:57:29 -0400

    Adjust webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,25 @@
 const path = require('path');
-const webpack = require("webpack");
-
-let plugins = []; // if using any plugins for both dev and production
-const devPlugins = []; // if using any plugins for development
-
-const prodPlugins = [
-  new webpack.DefinePlugin({
-    'process.env': {
-      'NODE_ENV': JSON.stringify('production')
-    }
-  }),
-  new webpack.optimize.UglifyJsPlugin({
-    compress: {
-      warnings: true
-    }
-  })
-];
-
-plugins = plugins.concat(
-  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
-);
+// const webpack = require("webpack");
+//
+// let plugins = []; // if using any plugins for both dev and production
+// const devPlugins = []; // if using any plugins for development
+//
+// const prodPlugins = [
+//   new webpack.DefinePlugin({
+//     'process.env': {
+//       'NODE_ENV': JSON.stringify('production')
+//     }
+//   }),
+//   new webpack.optimize.UglifyJsPlugin({
+//     compress: {
+//       warnings: true
+//     }
+//   })
+// ];
+//
+// plugins = plugins.concat(
+//   process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+// );
 
 module.exports = {
   context: __dirname,
@@ -28,7 +28,7 @@ module.exports = {
     path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
     filename: 'bundle.js'
   },
-  plugins: plugins,
+  // plugins: plugins,
   resolve: {
     extensions: ['.js', '.jsx', '*']
   },
